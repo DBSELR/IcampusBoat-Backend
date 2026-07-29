28f2b17507928f3d11558309e1982986cfa30db6
DBASE <89074505+DBSELR@users.noreply.github.com>
2026-07-29 11:25:07 +0530

    bbbbbbbbbbbbb

--- Models/Examinations/MarksEntryMHModel.cs
+++ Models/Examinations/MarksEntryMHModel.cs
@@ -27,12 +27,12 @@ namespace IcampusBoatBackend.Models.Examinations
         public string? Semester { get; set; }
         public string? Section { get; set; }
         public string? SubjectCode { get; set; }
+        public string? SubjectName { get; set; }
         public string? MidType { get; set; }
         public string? Date { get; set; }
         public string? UserId { get; set; }
         public List<StudentMarksMHItem>? Students { get; set; }
-        public string? stream { get; set; }
-
+        public string? Stream { get; set; }
     }
 
     public class StudentMarksMHItem
@@ -42,8 +42,13 @@ namespace IcampusBoatBackend.Models.Examinations
         public string? StudentName { get; set; }
         public string? HeadCode { get; set; }
         public string? HeadName { get; set; }
+        public string? SubMaxMrk { get; set; }
         public string? MaxMarks { get; set; }
         public string? Marks { get; set; }
+        public string? Grade { get; set; }
+        public string? CGPA { get; set; }
+        public string? SGPA { get; set; }
         public Dictionary<string, string>? HeadMarks { get; set; }
     }
 }
+
